643e30618dbe11ed39be698751fc89f57b51c2ec
JessicaLorenzon <jelorenzon@gmail.com>
2025-11-13 11:12:35 -0300

    desafio-04

--- java-spring-professional/desafio-consulta-vendas/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ java-spring-professional/desafio-consulta-vendas/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -16,13 +16,13 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
              SELECT new com.devsuperior.dsmeta.dto.ReportDTO(obj.id, obj.date, obj.amount, obj.seller.name)
              FROM Sale obj
              WHERE obj.date BETWEEN :minDate AND :maxDate
-             AND UPPER(obj.seller.name) LIKE UPPER(CONCATE('%', :name, '%'))
+             AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))
             """)
     List<ReportDTO> searchReport(@Param("minDate") LocalDate minDate,
                                  @Param("maxDate") LocalDate maxDate, @Param("name") String name);
 
     @Query("""
-            SELECT new com.devsuperior.dsmeta.dto.SumaryDTO (obj.seller.name, SUM(obj.amount))
+            SELECT new com.devsuperior.dsmeta.dto.SummaryDTO (obj.seller.name, SUM(obj.amount))
             FROM Sale obj
             WHERE obj.date BETWEEN :minDate AND :maxDate
             GROUP BY obj.seller.name
