4607272b65e5886c384050d28a122cb88e191342
JessicaLorenzon <jelorenzon@gmail.com>
2025-12-03 17:35:48 -0300

    acrescentando imgUrl em OrderItemDTO

--- java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,22 +8,25 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
     public OrderItemDTO() {
     }
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
-        this.productId = entity.getProduct().getId();
-        this.name = entity.getProduct().getName();
-        this.price = entity.getPrice();
-        this.quantity = entity.getQuantity();
+        productId = entity.getProduct().getId();
+        name = entity.getProduct().getName();
+        price = entity.getPrice();
+        quantity = entity.getQuantity();
+        imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getProductId() {
@@ -40,6 +43,10 @@ public class OrderItemDTO {
 
     public Integer getQuantity() {
         return quantity;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
     }
 
     public Double getSubTotal() {
