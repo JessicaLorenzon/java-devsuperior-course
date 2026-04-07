972b1a5e4df9f554c4b756dbbe9a661dec4925a9
JessicaLorenzon <jelorenzon@gmail.com>
2026-04-07 15:07:34 -0300

    add Factory

--- java-spring-expert/aula-junit/src/tests/entities/AccountTests.java
+++ java-spring-expert/aula-junit/src/tests/entities/AccountTests.java
@@ -3,15 +3,30 @@ package tests.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
     @Test
-    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+    public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
         //Arrange
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1L, 0.0);
+        Account acc = AccountFactory.createEmptyAccount();
+
+        //Act
+        acc.deposit(amount);
+
+        //Assert
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount() {
+        //Arrange
+        double expectedValue = 100.0;
+        Account acc = AccountFactory.createAccount(expectedValue);
+        double amount = -200.0;
 
         //Act
         acc.deposit(amount);
