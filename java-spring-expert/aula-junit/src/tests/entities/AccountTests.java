c96bc0ddf714b1f171a106ebe3cadc644ea6ae27
JessicaLorenzon <jelorenzon@gmail.com>
2026-04-07 15:29:07 -0300

    tests withdraw

--- java-spring-expert/aula-junit/src/tests/entities/AccountTests.java
+++ java-spring-expert/aula-junit/src/tests/entities/AccountTests.java
@@ -49,4 +49,25 @@ public class AccountTests {
         Assertions.assertTrue(expectedValue == acc.getBalance());
         Assertions.assertTrue(result == initialBalance);
     }
+
+    @Test
+    public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+        //Arrange
+        Account acc = AccountFactory.createAccount(800.0);
+
+        //Act
+        acc.withdraw(500.0);
+
+        //Assert
+        Assertions.assertEquals(300.0, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Account acc = AccountFactory.createAccount(800.0);
+            acc.withdraw(801.0);
+        });
+    }
 }
