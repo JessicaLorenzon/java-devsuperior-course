61c92ad60313c3b68d1458e0059c67e3f66b066f
JessicaLorenzon <jelorenzon@gmail.com>
2026-05-25 15:16:24 -0300

    teste busca de produto por id

--- java-spring-expert/dscommerce-jacoco/src/test/java/com/devsuperior/dscommerce/services/CategoryServiceTests.java
+++ java-spring-expert/dscommerce-jacoco/src/test/java/com/devsuperior/dscommerce/services/CategoryServiceTests.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,4 +46,5 @@ public class CategoryServiceTests {
         Assertions.assertEquals(result.size(), 1);
         Assertions.assertEquals(result.get(0).getId(), category.getId());
     }
+
 }
