f8ea186eb112e52075c01103238130a5c7b0adeb
jessicalorenzon <jelorenzon@gmail.com>
2026-09-02 17:55:04 -0300

    post user

--- java-spring-reference/nosql/workshop-springboot2-mongo4/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ java-spring-reference/nosql/workshop-springboot2-mongo4/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -27,8 +27,20 @@ public class UserService {
         return new UserDTO(entity);
     }
 
+    public UserDTO insert(UserDTO dto) {
+        User entity = new User();
+        copyDtoToEntity(dto, entity);
+        entity = repository.insert(entity);
+        return new UserDTO(entity);
+    }
+
     private User getEntityById(String id) {
         Optional<User> result = repository.findById(id);
         return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity) {
+        entity.setName(dto.getName());
+        entity.setEmail(dto.getEmail());
     }
 }
