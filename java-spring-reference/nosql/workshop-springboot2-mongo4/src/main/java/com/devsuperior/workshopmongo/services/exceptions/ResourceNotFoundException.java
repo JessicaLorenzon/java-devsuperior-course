2f022cff0824d7a448f56b807880929d0024aabb
jessicalorenzon <jelorenzon@gmail.com>
2026-09-02 17:48:12 -0300

    get user by id

--- java-spring-reference/nosql/workshop-springboot2-mongo4/src/main/java/com/devsuperior/workshopmongo/services/exceptions/ResourceNotFoundException.java
+++ java-spring-reference/nosql/workshop-springboot2-mongo4/src/main/java/com/devsuperior/workshopmongo/services/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,8 @@
 package com.devsuperior.workshopmongo.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
 
-	public ResourceNotFoundException(String msg) {
-		super(msg);
-	}
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
 }
