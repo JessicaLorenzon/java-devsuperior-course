f6ba39044ef39d4c750baec068020cc602ecbf6b
jessicalorenzon <jelorenzon@gmail.com>
2026-09-07 16:28:07 -0300

    get product by id

---
+++ java-spring-reference/nosql/workshop-springboot2-cassandra3/src/main/java/com/devsuperior/workshopcassandra/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.workshopcassandra.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.workshopcassandra.model.dto.ProductDTO;
+import com.devsuperior.workshopcassandra.model.entities.Product;
+import com.devsuperior.workshopcassandra.repositories.ProductRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public ProductDTO findById(UUID id) {
+		Product entity = getById(id);
+		return new ProductDTO(entity);
+	}
+	
+	private Product getById(UUID id) {
+		Optional<Product> result = repository.findById(id);
+		return result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+	}
+}
