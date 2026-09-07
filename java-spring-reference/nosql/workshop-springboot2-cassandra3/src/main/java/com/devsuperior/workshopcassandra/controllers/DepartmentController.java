063ce5ac1d1f65dfd9a5b8f6a2bffdf7d671e08c
jessicalorenzon <jelorenzon@gmail.com>
2026-09-07 16:04:21 -0300

    get all departments

---
+++ java-spring-reference/nosql/workshop-springboot2-cassandra3/src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -0,0 +1,34 @@
+package com.devsuperior.workshopcassandra.controllers;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
+import com.devsuperior.workshopcassandra.services.DepartmentService;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+	@Autowired
+	private DepartmentService service;
+	
+	@GetMapping
+	public ResponseEntity<List<DepartmentDTO>> findAll() {
+		List<DepartmentDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
