60dd7be5176c3315f8caa059f799a075f88ba3a9
jessicalorenzon <jelorenzon@gmail.com>
2026-09-07 15:55:55 -0300

    seed do banco de dados

---
+++ java-spring-reference/nosql/workshop-springboot2-cassandra3/src/main/java/com/devsuperior/workshopcassandra/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.workshopcassandra.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+
+import com.devsuperior.workshopcassandra.model.entities.Product;
+
+public interface ProductRepository extends CassandraRepository<Product, UUID> {
+
+}
