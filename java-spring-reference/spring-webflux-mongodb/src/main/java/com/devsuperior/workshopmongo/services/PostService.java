ad4a2d2011a02f9de42bb7c520a2975a31564be3
jessicalorenzon <jelorenzon@gmail.com>
2026-09-09 17:37:31 -0300

    get posts by title

--- java-spring-reference/spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ java-spring-reference/spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -5,6 +5,7 @@ import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
         return repository.findById(id)
                 .map(existingPost -> new PostDTO(existingPost))
                 .switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+    }
+
+    public Flux<PostDTO> findByTitle(String text) {
+        return repository.searchTitle(text).map(postFound -> new PostDTO(postFound));
     }
 }
