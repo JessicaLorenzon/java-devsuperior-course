6517a302dd39958957ec19267cc30103de6cc8b2
jessicalorenzon <jelorenzon@gmail.com>
2026-09-09 16:19:37 -0300

    update user

--- java-spring-reference/spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ java-spring-reference/spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -32,4 +32,9 @@ public class UserController {
                 .created(builder.path("/users/{id}").buildAndExpand(newUser.getId()).toUri())
                 .body(newUser));
     }
+
+    @PutMapping(value = "/{id}")
+    public Mono<ResponseEntity<UserDTO>> update(@PathVariable String id, @RequestBody UserDTO dto) {
+        return service.update(id, dto).map(userUpdated -> ResponseEntity.ok().body(userUpdated));
+    }
 }
