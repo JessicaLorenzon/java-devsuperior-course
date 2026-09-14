36081cd87c8881d27ffc6725c35b17f423675e24
jessicalorenzon <jelorenzon@gmail.com>
2026-09-14 18:01:57 -0300

    refactoring project

---
+++ java-spring-reference/spring-batch/hello-world-spring-batch/src/main/java/com/devsuperior/hello_world_spring_batch/tasklet/PrintHelloTasklet.java
@@ -0,0 +1,24 @@
+package com.devsuperior.hello_world_spring_batch.tasklet;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+public class PrintHelloTasklet implements Tasklet {
+
+    @Value("${name}")
+    private String name;
+
+    @Override
+    public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("Olá " + name + " !");
+        return RepeatStatus.FINISHED;
+    }
+}
