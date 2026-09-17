87dc092587852d12fdfcc9589bdfbc66dbc24881
jessicalorenzon <jelorenzon@gmail.com>
2026-09-17 18:16:40 -0300

    validating reading step

---
+++ java-spring-reference/spring-batch/user-request-spring-batch/user-request-sb/src/main/java/com/devsuperio/user_request_sb/writer/InsertUserDataDBWriterConfig.java
@@ -0,0 +1,15 @@
+package com.devsuperio.user_request_sb.writer;
+
+import com.devsuperio.user_request_sb.dto.UserDTO;
+import org.springframework.batch.infrastructure.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InsertUserDataDBWriterConfig {
+
+    @Bean
+    public ItemWriter<UserDTO> insertUserDataDBWriter() {
+        return users -> users.forEach(System.out::println);
+    }
+}
