cb1f91f92ae63f32f5cf5ee9c0976832e405d381
jessicalorenzon <jelorenzon@gmail.com>
2026-09-21 16:49:05 -0300

    basic configuration job and step

---
+++ java-spring-reference/spring-batch/send-book-email-spring-batch/src/main/java/com/devsuperior/send_book_email_spring_batch/step/SendEmailUserStepConfig.java
@@ -0,0 +1,29 @@
+package com.devsuperior.send_book_email_spring_batch.step;
+
+import com.devsuperior.send_book_email_spring_batch.domain.UserBookLoan;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.infrastructure.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class SendEmailUserStepConfig {
+
+    @Autowired
+    @Qualifier("transactionManagerApp")
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Step sendEmailUserStep(ItemReader<UserBookLoan> readUsersWithLoansCloseToReturnReader,
+                                  JobRepository jobRepository) {
+        return new StepBuilder("sendEmailUserStep", jobRepository)
+                .<UserBookLoan, UserBookLoan>chunk(1, transactionManager)
+                .reader(readUsersWithLoansCloseToReturnReader)
+                .build();
+    }
+}
